75865f209a9a6cccd406a62416c85dc89ed13a91
or man <Ormn1996@gmail.com>
2020-12-16 12:38:17 +0200

    property version update

--- CommonProject/commonMoudules/modules/PropertyListener.java
+++ CommonProject/commonMoudules/modules/PropertyListener.java
@@ -1,12 +1,21 @@
 package modules;
 
 
-/**class for functional programming for value change action, for more details see "Property&lt;T&gt;"*/ 
+/** class for functional programming for value change action, for more details see {@link Property}
+ * @author Or Man
+ * @version 1.1*/ 
 public abstract class PropertyListener<T> {
 
 	public PropertyListener() {
 	}
 	
-	public abstract void onChange(T oldVal,T newVal);
+	/***
+	 * Execute this method when the value of the property changed
+	 * @apiNote WARNING the parameter 'property' need to be used carefully, the use of setValue can cause loop,use {@link Property#silentSet(Object)} instead
+	 * @param property the {@link Property} that called this action
+	 * @param oldVal the last value of the property
+	 * @param newVal the new value of the property
+	 */
+	public abstract void onChange(Property<? extends T> property ,T oldVal,T newVal);
 
 }

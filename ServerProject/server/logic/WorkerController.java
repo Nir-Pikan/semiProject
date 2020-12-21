f6092d631b4de190f839b289970de3e300d39001
Nir Pikan <nirpikk@gmail.com>
2020-12-21 08:32:27 +0200

    JavaDoc Server-logic
    -DiscountController
    -ParkController
    -SubscriberController
    -WorkerController

--- ServerProject/server/logic/WorkerController.java
+++ ServerProject/server/logic/WorkerController.java
@@ -8,116 +8,158 @@ import io.DbController;
 import modules.IController;
 import modules.ServerRequest;
 
-public class WorkerController implements IController 
-{
-	DbController dbController ;
-	
-	/**Create worker table in DB if not exist*/
-	public WorkerController() 
-	{
+/** the worker controller class */
+public class WorkerController implements IController {
+	DbController dbController;
+
+	/** creates the {@link WorkerController} */
+	public WorkerController() {
 		dbController = DbController.getInstance();
 		createTable();
 	}
-	
-	private void createTable() 
-	{
+
+	/**
+	 * Create Worker table in DB if not exists
+	 * <p>
+	 * table of {@link Worker}s
+	 */
+	private void createTable() {
 		dbController.createTable("worker(FirstName varchar(20),LastName varchar(20),WorkerID varchar(12),"
 				+ "Email varchar(30),UserName varchar(20),WorkerType varchar(20),Password varchar(20),"
-				+ "isLogged varchar(4),permissions varchar(200),"
-				+ "primary key(UserName));"); 
+				+ "isLogged varchar(4),permissions varchar(200)," + "primary key(UserName));");
 	}
-		
-	/** Request: 1. (request.job = LogInWorker), (request.data = userName password) */
+
+	/**
+	 * Request: 1. (request.job = LogInWorker), (request.data = userName password)
+	 */
 	@Override
-	public String handleRequest(ServerRequest request) 
-	{	
-		if(request.job.equals("LogInWorker"))
-		{
+	public String handleRequest(ServerRequest request) {
+		if (request.job.equals("LogInWorker")) {
 			String[] parameters = request.data.split(" ");
 			String userName = parameters[0];
 			String password = parameters[1];
-			Worker worker = LogInWorker(userName, password);	
-			return ServerRequest.gson.toJson(worker,Worker.class);
+			Worker worker = LogInWorker(userName, password);
+			return ServerRequest.gson.toJson(worker, Worker.class);
 		}
 		return null;
 	}
-	
-	/** add worker to DB, return true if success*/
-	public boolean AddWorker(Worker worker) 
-	{
-      worker.setIsLogged(false);
-	  return dbController.sendUpdate("INSERT INTO worker() VALUES ("+worker.getFirstName()+","+worker.getLastName()+","+worker.getWorkerID()+","
-	  		+ worker.getEmail()+","+worker.getUserName()+","+worker.getWorkerType()+","
-	  		+ ""+worker.getPassword()+","+ParseIsLogginBoolToString(worker.getIsLogged())+","
-	        + ParsePermissionsToString(worker.getPermissions()) + ");");
+
+	/**
+	 * add worker to DB
+	 * 
+	 * @param worker the worker we want to add
+	 * @return true if success, false otherwise
+	 */
+	public boolean AddWorker(Worker worker) {
+		worker.setIsLogged(false);
+		return dbController.sendUpdate("INSERT INTO worker() VALUES (" + worker.getFirstName() + ","
+				+ worker.getLastName() + "," + worker.getWorkerID() + "," + worker.getEmail() + ","
+				+ worker.getUserName() + "," + worker.getWorkerType() + "," + "" + worker.getPassword() + ","
+				+ ParseIsLoginBoolToString(worker.getIsLogged()) + ","
+				+ ParsePermissionsToString(worker.getPermissions()) + ");");
 	}
-	
-	/** if this userName exist in DB and he has this password and he not log in return this worker and update that he log in, else return null*/
-	public Worker LogInWorker(String userName, String password)
-	{
-		ResultSet result = dbController.sendQuery("select * from worker where UserName=" + userName + " AND Password=" + password + ";");
-		try 
-		{
-			if(!result.next())
+
+	/**
+	 * if this userName exist in DB and he has this password and he is not logged in
+	 * return this worker and update his logged in status, else return null
+	 * 
+	 * @param userName login user name
+	 * @param password login password
+	 * @return if successes return logged in worker, null otherwise
+	 */
+	public Worker LogInWorker(String userName, String password) {
+		ResultSet result = dbController
+				.sendQuery("select * from worker where UserName=" + userName + " AND Password=" + password + ";");
+		try {
+			if (!result.next())
 				return null;
 			String FirstName = result.getString(1);
 			String LastName = result.getString(2);
 			String WorkerID = result.getString(3);
 			String Email = result.getString(4);
 			String WorkerType = result.getString(6);
-			boolean isLogged = ParseIsLogginStringToBool(result.getString(8));
-			Permissions permissions = ParseStringToPermissions(result.getString(9), WorkerID);
-			if(isLogged)
+			boolean isLogged = ParseIsLoginStringToBool(result.getString(8));
+			Permissions permissions = ParseStringToPermissions(result.getString(9));
+			if (isLogged)
 				return null;
 			isLogged = true;
-			Worker worker = new Worker(userName, FirstName, LastName, WorkerID, Email, WorkerType, password, isLogged, permissions);		
-			if(!updateWorkerLogginDB(worker))
+			Worker worker = new Worker(userName, FirstName, LastName, WorkerID, Email, WorkerType, password, isLogged,
+					permissions);
+			if (!updateWorkerLogginDB(worker))
 				return null;
-			return worker;		
-		} 
-		catch (Exception e) 
-		{
+			return worker;
+		} catch (Exception e) {
 			return null;
-		} 
+		}
 	}
-	
-	/** update the worker log in status in DB (worker.IsLogged property need to be updated before), return true if success*/
-	public boolean updateWorkerLogginDB(Worker worker) 
-	{
-		return dbController.sendUpdate("UPDATE worker SET isLogged=" + ParseIsLogginBoolToString(worker.getIsLogged()) + " WHERE UserName=" + worker.getUserName());
+
+	/**
+	 * update the worker log in status in DB
+	 * <p>
+	 * (worker.IsLogged property need to be updated before), return true if success
+	 * 
+	 * @param worker the worker we want to update
+	 * @return true if success, false otherwise
+	 */
+	public boolean updateWorkerLogginDB(Worker worker) {
+		return dbController.sendUpdate("UPDATE worker SET isLogged=" + ParseIsLoginBoolToString(worker.getIsLogged())
+				+ " WHERE UserName=" + worker.getUserName());
 	}
-	
-	private String ParsePermissionsToString(Permissions permissions)
-	{
-		String output = "";
-		for (Permission permission : permissions.GetPermissions()) 
-		{
+
+	/**
+	 * converts {@link Permissions} into string
+	 * 
+	 * @param permissions the {@link Permissions} we want to convert
+	 * @return the output String
+	 */
+	private String ParsePermissionsToString(Permissions permissions) {
+		String output = permissions.GetParkID();
+		for (Permission permission : permissions.GetPermissions()) {
 			output = output + " " + permission.GetName();
 		}
 		return output;
 	}
-	
-	private Permissions ParseStringToPermissions(String permissionsString, String WorkerID)
-	{
-		Permissions permissions = new Permissions(WorkerID);
+
+	/**
+	 * converts a string to a {@link Permissions} and add them to wanted
+	 * {@link Worker}
+	 * 
+	 * @param permissionsString the String we want to convert
+	 * @return the {@link Permissions} made using the String
+	 * @apiNote the start of the String should contain the ParkID to be used
+	 * @apiNote each {@link Permissions} should be split via space
+	 */
+	private Permissions ParseStringToPermissions(String permissionsString) {
 		permissionsString = permissionsString.trim();
 		String[] permissionsStringArray = permissionsString.split(" ");
-		for (String permission : permissionsStringArray) 
-		{
-			permissions.AddPermission(new Permission(permission));
+		String ParkID = permissionsStringArray[0];
+		Permissions permissions = new Permissions(ParkID);
+		for (String permission : permissionsStringArray) {
+			if (!permission.equals(ParkID))
+				permissions.AddPermission(new Permission(permission));
 		}
 		return permissions;
 	}
-	
-	private boolean ParseIsLogginStringToBool(String logginString)
-	{
-		return logginString.equals("YES");		
+
+	/**
+	 * checks if login String is Yes
+	 * 
+	 * @param loginString the string we want to check
+	 * @return true if YES, false otherwise
+	 */
+	private boolean ParseIsLoginStringToBool(String loginString) {
+		return loginString.equals("YES");
 	}
-	
-	private String ParseIsLogginBoolToString(Boolean logginBool)
-	{
-		if(logginBool)
-		  return "YES";
+
+	/**
+	 * converts boolean to login String
+	 * 
+	 * @param loginBool the boolean we want to convert
+	 * @return YES if true, NO if false
+	 */
+	private String ParseIsLoginBoolToString(Boolean loginBool) {
+		if (loginBool)
+			return "YES";
 		return "NO";
 	}
 }

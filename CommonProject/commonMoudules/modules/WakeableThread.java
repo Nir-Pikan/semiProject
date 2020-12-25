0222dfcece3cbe59607ba7ee1a48c3cafcfa4e94
or man <Ormn1996@gmail.com>
2020-12-25 04:03:33 +0200

    waiting List

---
+++ CommonProject/commonMoudules/modules/WakeableThread.java
@@ -0,0 +1,61 @@
+package modules;
+
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
+public class WakeableThread extends Thread {
+
+	public static long TIME_ACCURECY = 1000;
+	
+	private boolean needToWake = false;
+	private Object lock = new Object();
+	
+	public WakeableThread(Runnable arg0) {
+		super(arg0);
+	}
+
+	public void sleepUntillWoken() {
+		needToWake = false;
+		synchronized (lock) {
+			while(!needToWake) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+		
+	}
+	
+	public boolean sleepUntillWoken(LocalTime endTime) {
+		needToWake = false;
+		synchronized (lock) {
+			while(!needToWake && LocalTime.now().isBefore(endTime)) {
+				try {
+					wait(TIME_ACCURECY);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+		return needToWake;
+	}
+	
+	public boolean sleepUntillWoken(TimeUnit deltaUnit, long delta) {
+		needToWake = false;
+		LocalTime endTime = LocalTime.now().plusSeconds(deltaUnit.toSeconds(delta));
+		synchronized (lock) {
+			while(!needToWake && LocalTime.now().isBefore(endTime)) {
+				try {
+					wait(TIME_ACCURECY);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+		return needToWake;
+	}
+	
+	public void wake() {
+		needToWake = true;
+		lock.notify();
+	}
+}
--- ServerProject/server/logic/WaitingListController.java
+++ ServerProject/server/logic/WaitingListController.java
@@ -1,28 +1,255 @@
 package logic;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import entities.Order;
+import entities.Order.IdType;
+import entities.Order.OrderStatus;
+import io.DbController;
 import modules.IController;
+import modules.PeriodicallyRunner;
 import modules.ServerRequest;
+import modules.WakeableThread;
 
 
 public class WaitingListController implements IController {
 	private OrderController order; 
 	private MessageController messageC;
 	
-	
+	private DbController dbController;
+	private Map<Integer,WakeableThread> currentWaitingCancelation;//the order who are we waiting for
+	private Map<WakeableThread,Order> currentCancelation;//the canceled order of the thread 
 
+	
+	private static int AvgVisitTime = 4; //TODO change to park visit Time
+	
+	
 	public WaitingListController(OrderController order, MessageController messageC) {
 		super();
 		this.order = order;
 		this.messageC = messageC;
-		// TODO Auto-generated constructor stub
+		dbController = DbController.getInstance();
+		currentWaitingCancelation = new HashMap<Integer, WakeableThread>();
+		currentCancelation = new HashMap<WakeableThread, Order>();
+		createTable();
+		
+		order.canceled.getAddProperty().AddListener((obs,oldVal,newVal)->{
+			 WakeableThread t = new WakeableThread(()->{
+				 Thread cur = Thread.currentThread();
+				 if(!(cur instanceof WakeableThread)) 
+					 return;
+				 WakeableThread myThread = (WakeableThread )cur;
+				 handelCancel(newVal, myThread);
+				 currentCancelation.remove(myThread);//finish with the cancellation
+				});
+			 currentCancelation.put(t, newVal);
+			});
+		InitWaitingListCleaner();
 	}
 
+	private void InitWaitingListCleaner() {
+		PeriodicallyRunner.runEveryDayAt(23, 00, ()->{
+			dbController.sendUpdate("");
+			PreparedStatement pstmt = dbController.getPreparedStatement("DELETE FROM waitingList WHERE visitTime < ?;");
+			
+			Timestamp now = Timestamp.valueOf(LocalDateTime.now());
+			try {
+				pstmt.setTimestamp(1, now);
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+			}
+		});
+	}
+	
+	/**
+	 * Create waitingList table in DB if not exists
+	 */
+	private void createTable() {
+		// please consider that type (enum value) name was changed from IDtype to type,
+		// hope no bug is will be found
+		boolean isCreated = dbController.createTable("waitingList(parkSite varchar(20),numberOfVisitors int,orderID int,"
+				+ "priceOfOrder FLOAT, email varchar(20),phone varchar(20), type ENUM('PRIVATE','PRIVATEGROUP','GUIDE','FAMILY'),"
+				+ "orderStatus ENUM('CANCEL','SEMICANCEL','IDLE','CONFIRMED','WAITINGLIST','WAITINGLISTMASSAGESENT'),"
+				+ "visitTime TIMESTAMP(1), timeOfOrder TIMESTAMP(1), isUsed BOOLEAN,ownerID varchar(20), primary key(orderID));");
+		if (isCreated)
+			System.out.println("Table has been created");
+	}
+	
+	private void handelCancel(Order canceled,WakeableThread thread){
+		canceled.orderStatus = OrderStatus.SEMICANCELED;
+		order.UpdateOrder(canceled);
+		Order nextWaiting;
+		while((nextWaiting = getNextOrder(canceled))!=null) {
+			notifyWaitingOrder(nextWaiting);
+			
+			if(thread.sleepUntillWoken(TimeUnit.HOURS, 1)) {
+				//if woken by user interaction
+				if(!currentCancelation.containsKey(thread))
+					return;//exit if finished with this cancellation
+				
+			}else {
+				//if user did not response
+				deleteFromWaitingList(nextWaiting);
+				currentWaitingCancelation.remove(nextWaiting.orderID);
+			}
+			
+		}
+		
+		canceled.orderStatus = OrderStatus.CANCEL;
+		order.UpdateOrder(canceled);
+	}
+	
+	
+	private boolean AddNewOrderTowaitingList(Order ord) {
+		PreparedStatement ps = dbController
+				.getPreparedStatement("INSERT INTO orders VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+		try {
+			ps.setString(1, ord.parkSite);
+			ps.setInt(2, ord.numberOfVisitors);
+			ps.setLong(3, ord.orderID);
+			ps.setFloat(4, ord.priceOfOrder);
+			ps.setString(5, ord.email);
+			ps.setString(6, ord.phone);
+			ps.setString(7, ord.type.toString()); 
+			ps.setString(8, ord.orderStatus.toString());
+			ps.setTimestamp(9, ord.visitTime);
+			ps.setTimestamp(10, ord.timeOfOrder);
+			ps.setBoolean(11, ord.isUsed);
+			ps.setString(12, ord.ownerID);
 
+			return ps.executeUpdate() == 1;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private void notifyWaitingOrder(Order nextWaiting) {
+		messageC.SendEmailAndSMS(nextWaiting.email, nextWaiting.phone,
+				"Your Order No: "+nextWaiting.orderID+ " can be accepted,\n"//content
+						+ " Please Go to GoNature and approve/cancel this order."
+						+ "This offer is for one hour only!",
+						"Order "+nextWaiting.orderID+" out if waiting list");//subject
+		
+	}
+
+	private void deleteFromWaitingList(Order order) {
+		PreparedStatement pstmt = dbController.getPreparedStatement("DELETE FROM waitingList WHERE orderID = ?;");
+		try {
+			pstmt.setInt(1, order.orderID);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(pstmt.toString());
+			System.out.println("Failed to delete order");
+		}
+		
+	}
+
+	/**
+	 * get next and set to messasge sent
+	 * @param canceled
+	 * @return
+	 */
+	private synchronized Order getNextOrder(Order canceled) {
+		Timestamp minimum = Timestamp.valueOf(canceled.visitTime.toLocalDateTime().minusSeconds(TimeUnit.HOURS.toSeconds(AvgVisitTime-1)));
+		Timestamp maximum = Timestamp.valueOf(canceled.visitTime.toLocalDateTime().plusSeconds(TimeUnit.HOURS.toSeconds(AvgVisitTime)));
+		 PreparedStatement ps = dbController.getPreparedStatement("SELECT * FROM waitingList WHERE (timestamp(visitTime) >= timestamp( ? )  \"\r\n" + 
+				" and  timestamp(visitTime) <= timestamp( ? ) AND parkSite = ? And orderStatus = 'WAITINGLIST' ORDER BY timeOfOrder ASC;");
+		
+		List<Order> list = new ArrayList<>();
+		try {
+			ps.setTimestamp(1, minimum);
+			ps.setTimestamp(2, maximum);
+			ps.setString(3, canceled.parkSite);
+			
+			ResultSet res = ps.executeQuery();
+			if (res.next()) {
+				list.add(new Order(res.getString(1), res.getInt(2), res.getInt(3), res.getFloat(4), res.getString(5),
+						res.getString(6), IdType.valueOf(res.getString(7)), OrderStatus.valueOf(res.getString(8)),
+						res.getTimestamp(9), res.getTimestamp(10), res.getBoolean(11), res.getString(12)));
+			}
+			res.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		for(Order o : list) {
+//			if(order.isOrderAllowedWaitingList(o,canceled.numberOfVisitors)
+//			{//settimg the Order to sent message
+//			dbController.sendUpdate("UPDATE waitingList SET orderStatus='WAITINGLISTMASSAGESENT' WHARE orderID = "+o.orderID+";");
+//				return o;
+//		}
+//			TODO remove after creation of method
+		}
+		return null;
+	}
+	
+	private void acceptWaitingOrder(int orderId) {
+		//set canceled Order as canceled 
+				WakeableThread waitingT = currentWaitingCancelation.get(orderId);
+				Order canceled = currentCancelation.get(waitingT);
+				canceled.orderStatus = OrderStatus.CANCEL;
+				order.UpdateOrder(canceled);
+				
+				Order acceptedOrder = getwaitingOrder(orderId);
+				
+		acceptedOrder.orderStatus = OrderStatus.IDLE;
+				order.AddNewOrder(acceptedOrder);
+				deleteFromWaitingList(acceptedOrder);
+				
+			currentWaitingCancelation.remove(orderId);
+		waitingT.wake();
+	}
+
+	private Order getwaitingOrder(int orderId) {
+		ResultSet res = dbController.sendQuery("SELECT * FROM waitingList WHERE orderID = \"" + orderId + "\";");
+		
+		Order o = null;
+		try {
+			if (res.next()) {
+				o = new Order(res.getString(1), res.getInt(2), res.getInt(3), res.getFloat(4), res.getString(5),
+						res.getString(6), IdType.valueOf(res.getString(7)), OrderStatus.valueOf(res.getString(8)),
+						res.getTimestamp(9), res.getTimestamp(10), res.getBoolean(11), res.getString(12));
+			}
+			res.close();
+			return o;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	@Override
 	public String handleRequest(ServerRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		switch(request.job) {
+		case "acceptWaitingOrder":
+			acceptWaitingOrder(Integer.parseInt(request.data));
+			return "accepted";
+			
+		case "cancelWaitingOrder":
+			deleteFromWaitingList(getwaitingOrder(Integer.parseInt(request.data)));
+			WakeableThread waitingT = currentWaitingCancelation.get(Integer.parseInt(request.data));
+			if(waitingT!= null)
+				waitingT.wake();
+			return "canceled";
+		case "addToWaitingList":
+			AddNewOrderTowaitingList(ServerRequest.gson.fromJson(request.data, Order.class));
+			return "added";
+		default:
+			return "unsupported request";
+		}
+		
 	}
 
 }

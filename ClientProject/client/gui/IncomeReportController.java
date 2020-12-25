691ef5283666536b117b8e9d68f0ba8c6e47138d
aviv <avivvanun@walla.co.il>
2020-12-25 03:53:55 +0200

    Reports And LoginController

--- ClientProject/client/gui/IncomeReportController.java
+++ ClientProject/client/gui/IncomeReportController.java
@@ -1,5 +1,19 @@
 package gui;
 
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.TreeMap;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+import entities.Order;
+import entities.Order.OrderStatus;
+import io.clientController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 /**
@@ -7,21 +21,27 @@ import javafx.event.ActionEvent;
  */
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import mocks.Card;
 import module.GuiController;
+import modules.ServerRequest;
+import modules.ServerRequest.Manager;
 
 /** the IncomeReport page controller */
-public class IncomeReportController implements GuiController{
+public class IncomeReportController implements GuiController, Initializable{
 
     @FXML
     private Label labelDateToday;
 
     @FXML
-    private TextField textDateToday;
+    private Label textDateToday;
 
     @FXML
     private Label labelIncomeReport;
@@ -30,16 +50,16 @@ public class IncomeReportController implements GuiController{
     private Label labalParkName;
 
     @FXML
-    private TextField textParkName;
+    private Label textParkName;
 
     @FXML
     private Label labaelReportDate;
 
     @FXML
-    private TextField textReportDate;
+    private Label textReportDate;
 
     @FXML
-    private TextField textTotalIncome;
+    private Label textTotalIncome;
 
     @FXML
     private Label labelTotalIncome;
@@ -48,26 +68,144 @@ public class IncomeReportController implements GuiController{
     private Label labelAVGIncomeDay;
 
     @FXML
-    private TextField textAVGIncomeDay;
+    private Label textAVGIncomeDay;
 
     @FXML
-    private TableView<?> incomeTable;
+    private TableView<Cell> incomeTable;
 
     @FXML
-    private TableColumn<?, ?> dateColumn;
+    private TableColumn<Cell, Integer> dateColumn;
 
     @FXML
-    private TableColumn<?, ?> visitorsColumn;
+    private TableColumn<Cell, Integer> visitorsColumn;
 
     @FXML
-    private TableColumn<?, ?> incomeColumn;
+    private TableColumn<Cell, Integer> incomeColumn;
 
     @FXML
     private Button buttonPrint;
 
-    @FXML
-    void buttonPrint_OnClick(ActionEvent event) {
-
+    private int TotalIncome = 0;    
+    
+    private Timestamp[] reportStartAndEndTimes;
+    private String parkName;
+    
+    /**
+     * impotant Notice:
+     * @param parkName
+     * @param reportStartAndEndTimes
+     */ 
+    public IncomeReportController(String parkName, Timestamp[] reportStartAndEndTimes)            
+    {
+    	this.reportStartAndEndTimes = reportStartAndEndTimes;
+    	this.parkName = parkName;
     }
+    
+    @FXML
+    void buttonPrint_OnClick(ActionEvent event)
+    {
+    	TotalIncome = 0;
+    	ServerRequest serverRequest = new ServerRequest(Manager.Order, "GetAllListOfOrders", "");
+    	String response = clientController.client.sendRequestAndResponse(serverRequest);
+    	Order[] allOrders = ServerRequest.gson.fromJson(response, Order[].class);
+    	if(allOrders == null)
+    		return; 
+    	Map<Integer, ArrayList<Order>> map = new TreeMap<Integer, ArrayList<Order>>();
+    	for (Order order : allOrders) 
+    	{
+    		if(ValidOrderToReport(order)) 
+    		{
+    			int day = order.visitTime.getDay();
+    			if(!map.containsKey(day))
+    			{
+    				map.put(day, new ArrayList<Order>());
+    			}
+    			map.get(day).add(order);
+    		}
+		}
+    	for (Map.Entry<Integer, ArrayList<Order>> entry : map.entrySet())
+    	{
+			int income = 0;
+			int visitors = 0;
+			int day = entry.getKey();
+			
+			for (Order order : entry.getValue()) 
+			{
+				visitors += order.numberOfVisitors;
+				income += order.priceOfOrder;
+			}
+			TotalIncome += income;
+				
+			incomeTable.getItems().add(new Cell(income, visitors, day));
+		}
+    	Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+    	textDateToday.setText(GetStringTime(currentTime));
+    	textParkName.setText(parkName);
+    	textReportDate.setText(GetStringTime(reportStartAndEndTimes[0]) + " - " + GetStringTime(reportStartAndEndTimes[1]));
+    	textTotalIncome.setText(TotalIncome + ""); 
+    	textAVGIncomeDay.setText((TotalIncome/map.size()) + "");
+    }
+    
+    public String GetStringTime(Timestamp time)
+    {
+    	String[] arr = time.toString().split(" ")[0].split("-");
+    	return arr[2] + "." + arr[1] + "." + arr[0];
+    }
+    
+    private boolean ValidOrderToReport(Order order)
+    {
+    	//// need also to check if it is from the last month ///////////////////////////////////////////////////////////////
+      	if(!order.visitTime.before(new Timestamp(System.currentTimeMillis())))
+			return false; 		
+	    if( order.orderStatus.equals(OrderStatus.CANCEL) || order.orderStatus.equals(OrderStatus.SEMICANCELED))
+	    	return false; 
+	    return true;
+    }
+    
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) { // needed for appropriate working of TableView
+    	dateColumn.setCellValueFactory(new PropertyValueFactory<Cell,Integer>("day"));
+    	visitorsColumn.setCellValueFactory(new PropertyValueFactory<Cell,Integer>("visitors"));
+    	incomeColumn.setCellValueFactory(new PropertyValueFactory<Cell,Integer>("price"));
+	}
 
+    public class Cell
+    {
+    	public Integer price;
+    	public Integer visitors;
+    	public Integer day;
+    	
+    	public Integer getVisitors() {
+			return visitors;
+		}
+
+		public void setVisitors(Integer visitors) {
+			this.visitors = visitors;
+		}
+
+		public Integer getDay() {
+			return day;
+		}
+
+		public void setDay(Integer day) {
+			this.day = day;
+		}
+
+		public Integer getPrice() {
+			return price;
+		}
+
+		public void setPrice(Integer price) {
+			this.price = price;
+		}
+
+		public Cell(Integer price,Integer visitors , Integer day)
+    	{
+			this.day = day;
+			this.visitors = visitors;
+    		this.price = price;
+    	}
+    }
+		
+	
 }
--- ClientProject/client/gui/LoginController.java
+++ ClientProject/client/gui/LoginController.java
@@ -1,5 +1,9 @@
 package gui;
 
+import entities.Subscriber;
+import entities.Worker;
+
+import io.clientController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -8,9 +12,14 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import module.GuiController;
+import module.Navigator;
+import module.PopUp;
+import modules.Property;
+import modules.ServerRequest;
+import modules.ServerRequest.Manager;
 
-/** the Login page controller */
-public class LoginController implements GuiController {
+public class LoginController implements GuiController 
+{
 
 	@FXML
 	private GridPane visitorLoginForm;
@@ -31,25 +40,24 @@ public class LoginController implements GuiController {
 	private TextField txtUsername;
 
 	@FXML
-	private PasswordField txtPassword;
+    private PasswordField txtPassword;
 
 	@FXML
 	private CheckBox cbUserWorker;
+	
 
 	@FXML
-	void UserLogin(ActionEvent event) {
-
-	}
-
-	/** when clicking on the user/worker check box hide/reveal wanted fields */
-	@FXML
-	void UserWorkerCheckBox(ActionEvent event) {
-		if (cbUserWorker.isSelected()) {
+	void UserWorkerCheckBox(ActionEvent event) 
+	{
+		if (cbUserWorker.isSelected()) 
+		{
 			visitorLoginForm.setManaged(false);
 			visitorLoginForm.setVisible(false);
 			workerLoginForm.setManaged(true);
 			workerLoginForm.setVisible(true);
-		} else {
+		} 
+		else
+		{
 			visitorLoginForm.setManaged(true);
 			visitorLoginForm.setVisible(true);
 			workerLoginForm.setManaged(false);
@@ -58,14 +66,96 @@ public class LoginController implements GuiController {
 	}
 
 	@FXML
-	void WorkerLogin(ActionEvent event) {
-
+	void WorkerLogin(ActionEvent event)
+	{
+		String userName = txtUsername.getText();
+		String password = txtPassword.getText();
+		ServerRequest serverRequest = new ServerRequest(Manager.Worker,
+				"LogInWorker", userName + " " + password);
+		String response = clientController.client.sendRequestAndResponse(serverRequest);
+		Worker worker = ServerRequest.gson.fromJson(response, Worker.class);
+		if(worker == null)
+		{
+			PopUp.showError("Sign up error", "Faild to log in", "Please check the user name and the password");
+			return;
+		}
+		clientController.client.logedInWorker.setVal(worker);
+		Navigator.instance().clearHistory();
 	}
-
-	public void init() {
+	
+	@FXML
+	void UserLogin(ActionEvent event) 
+	{
+		String userID = txtId.getText();
+		if(isSubscriberID(userID))
+		{
+			LoginSubscriber(userID, "This subscriber ID not exist \nPlease check the input", true);
+			return;		
+		}
+		if(isID(userID))
+		{
+			 if(LoginSubscriber("S"+userID,"",false))
+				 return;
+			 clientController.client.visitorID.setVal(userID);
+			 Navigator.instance().clearHistory();
+			 return;
+		}
+		PopUp.showError("Error", "Faild to identify", "Please check the input:\nID: 9 digit number\nSubscriber ID: need to start with 'S'");
+	}
+	
+	//return true if success to log in the subscriber
+	private boolean LoginSubscriber(String subscriberID, String ErrorMessageForPopUp, boolean needPopUpForFail)
+	{
+		ServerRequest serverRequest = new ServerRequest(Manager.Subscriber,
+				"GetSubscriberData", subscriberID);
+		String response = clientController.client.sendRequestAndResponse(serverRequest);
+		if(response.endsWith("not found"))
+		{
+			if(needPopUpForFail)
+			     PopUp.showError("Sign up error", "Faild to identify", ErrorMessageForPopUp);
+			return false;
+		}
+		Subscriber subscriber = ServerRequest.gson.fromJson(response, Subscriber.class);
+		if(subscriber == null)
+		{
+			if(needPopUpForFail)
+			    PopUp.showError("Sign up error", "Faild to identify", ErrorMessageForPopUp);
+			return false;
+		}
+		clientController.client.logedInSunscriber.setVal(subscriber);
+		Navigator.instance().clearHistory();
+		return true;
+	}
+	
+	private boolean isSubscriberID(String idString)
+	{
+		if(idString.length() > 0 && idString.charAt(0) == 'S')
+			return true;
+		return false;
+	}
+	
+	private boolean isID(String idString)
+	{
+		if(idString.length()!= 9)
+			return false;
+		for (char num : idString.toCharArray())
+		{
+			if(!isNumber(num))
+				return false;
+		}
+		return true;
+	}
+	
+	private boolean isNumber(char num)
+	{
+		return (0 <= num - '0') && (9 >= num - '0');
+	}
+	
+	
+	public void init()
+	{
 		workerLoginForm.setManaged(false);
-		workerLoginForm.setVisible(false);
-
+		workerLoginForm.setVisible(false);	
 	}
 
 }

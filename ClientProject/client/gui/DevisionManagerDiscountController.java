dd22115457fa16eb3ca7c4c9cf7064003073f9db
MichaelGindin <mihael1023@gmail.com>
2020-12-31 11:19:19 +0200

    undo

--- ClientProject/client/gui/DevisionManagerDiscountController.java
+++ ClientProject/client/gui/DevisionManagerDiscountController.java
@@ -43,7 +43,7 @@ public class DevisionManagerDiscountController implements GuiController {
 
 	@Override
 	public void init() {
-		discountsTable.getStyleClass().add("michael-tree-table-view");
+		discountsTable.getStyleClass().add("table-view-M");
 		discountID.setCellValueFactory(new PropertyValueFactory<DiscountEntity, String>("DiscountID"));
 		discountInPercentage.setCellValueFactory(new PropertyValueFactory<DiscountEntity, Float>("DiscountValue"));
 		discountStartDate.setCellValueFactory(new PropertyValueFactory<DiscountEntity, LocalDate>("startDateShow"));

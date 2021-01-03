49e06b6e6a5c12c9ee0e27205699b2e48d362ddb
or man <Ormn1996@gmail.com>
2021-01-03 13:12:07 +0200

    javadoc motd

--- ClientProject/client/module/MovingMessage.java
+++ ClientProject/client/module/MovingMessage.java
@@ -1,24 +1,30 @@
 package module;
 
-
-
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
-import javafx.scene.layout.Background;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.util.Duration;
 
+/**
+ * class that extends textFlow, shows messages moving vertically across scrollPane<br>
+ * use {@link #setMessages(String[])} to set the messages of this pane<br>
+ * CSS - Text class=(text-id)
+ *
+ */
 public class MovingMessage extends TextFlow{
-
+private final DoubleProperty screenHight;
 	Timeline t;
+	
+	/**
+	 * create moving message and apply it to the given ScrollPane
+	 * @param scroll {@link ScrollPane} the container of the message
+	 */
 	public MovingMessage( ScrollPane scroll){
 		super();
 		scroll.setVbarPolicy(ScrollBarPolicy.NEVER);
@@ -36,15 +42,23 @@ public class MovingMessage extends TextFlow{
 		});
 		scroll.setContent(this);
 		this.setLineSpacing(10);
+		screenHight = new SimpleDoubleProperty(scroll.getPrefHeight());
 	}
 	
-	public void setMessages(String[] messges) {
+	/**
+	 * set the messages for the moving message pane
+	 * @param messages messages to show
+	 */
+	public void setMessages(String[] messages) {
+		if(messages == null)
+			return;
 		this.getChildren().clear();
-		for(String s : messges) {
+		for(String s : messages) {
 			Text t = new Text(s+"\n");
 			t.getStyleClass().add("text-id");
 			this.getChildren().add(t);
 		}
+		t.rateProperty().bind(screenHight.divide(this.heightProperty()));
 	}
 	
 }

3917ad3dafb47e0b0a3d6983bff9b0cf5d466f74
Nir Pikan <nirpikk@gmail.com>
2021-01-07 20:13:48 +0200

    Server project - Java doc (no logic package)

--- ServerProject/server/gui/ServerGuiController.java
+++ ServerProject/server/gui/ServerGuiController.java
@@ -1,9 +1,6 @@
 package gui;
 
-import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -15,12 +12,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import modules.ConsoleReplacer;
 import util.UIupdate;
 
-public class ServerGuiController implements Initializable{
+/**
+ * the server's gui controller class
+ */
+public class ServerGuiController implements Initializable {
 
 	/** main window wont close if this is true */
 	public static boolean isServerRunning = false;
@@ -44,13 +43,16 @@ public class ServerGuiController implements Initializable{
 	@FXML
 	private TextField serverPort;
 
-	   @FXML
-	    private ScrollPane scroll;
+	@FXML
+	private ScrollPane scroll;
 
 	@FXML
+	/**
+	 * starts running the server
+	 */
 	void StartServer(ActionEvent event) {
 		int port;
-		
+
 		if (serverPort.getText().contentEquals("")) {
 			port = ServerController.DEFAULT_SERVER_PORT;
 			serverPort.setText(Integer.toString(ServerController.DEFAULT_SERVER_PORT));
@@ -68,6 +70,9 @@ public class ServerGuiController implements Initializable{
 	}
 
 	@FXML
+	/**
+	 * shuts the server down
+	 */
 	void closeAll(ActionEvent event) {
 		try {
 			sc.stopListening();
@@ -80,25 +85,36 @@ public class ServerGuiController implements Initializable{
 		}
 	}
 
-	public void setConnected(String address,String hostname) {
+	/**
+	 * update server's status when connected to host
+	 * 
+	 * @param address  the address the server connects to
+	 * @param hostname the host the server connects to
+	 */
+	public void setConnected(String address, String hostname) {
 		UIupdate.update(() -> {
 			txtIp.setText(address);
 			txtHost.setText(hostname);
 			connectionStatus.setText("Connected");
 		});
-		
+
 	}
 
+	/**
+	 * update server's status when disconnecting from host
+	 */
 	public void setDisconnected() {
 		UIupdate.update(() -> {
 			txtIp.setText("not Connected");
 			txtHost.setText("not Connected");
 			connectionStatus.setText("not Connected");
 		});
-		
 
 	}
 
+	/**
+	 * update DB status when connecting to DB
+	 */
 	public void dbConnected() {
 		DbStatus.setText("connected");
 
@@ -108,7 +124,5 @@ public class ServerGuiController implements Initializable{
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		new ConsoleReplacer(scroll).replaceAll(true);
 	}
-	
-	
 
 }
